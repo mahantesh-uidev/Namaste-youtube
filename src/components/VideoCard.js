7ecb5cb1efc1ceff3c93ab4366143d018d89716a
mahantesh-uidev <mahantesh.uideveloper@gmail.com>
2024-09-01 23:19:25 +0530

    added view count helper function and published time of video to videoCard component

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { valueConverter } from '../utils/helper';
+import moment from 'moment';
 
 const VideoCard = ({info}) => {
     const {snippet, statistics} = info;
-    const {channelTitle, title, thumbnails} = snippet;
+    const {channelTitle, title, thumbnails, publishedAt} = snippet;
   return (
     <div className='p-2 m-2 w-64 shadow-lg'>
         <img className='rounded-lg' src={thumbnails.medium.url} alt='thumbnail' />
         <ul>
             <li className='font-bold py-2'>{title}</li>
             <li>{channelTitle}</li>
-            <li>{statistics.viewCount} views</li>
+            <li>{valueConverter(statistics.viewCount)} views &bull; {moment(publishedAt).fromNow()}</li>
         </ul>
     </div>
   )
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -38,3 +38,13 @@ export function makeRandomMessage(length) {
     }
     return result;
 }
+
+export function valueConverter(value) {
+    if(value >= 1000000){
+        return Math.floor(value/1000000) + "M";
+    }else if(value >= 1000){
+        return Math.floor(value/1000) + "K";
+    }else {
+        return value;
+    }
+}
